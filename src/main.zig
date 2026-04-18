70c5ad6c580659ba7cf63f732b4efadf54cfae7e
Jackson Otto <73965396+JacksonO123@users.noreply.github.com>
2026-04-17 20:50:15 -0700

    Improve resizing rendering (#1)
    * fix resizing problems

--- src/main.zig
+++ src/main.zig
@@ -2,24 +2,30 @@ const std = @import("std");
 
 const zig_tui = @import("zig_tui");
 
-const renderer = @import("renderer.zig");
-const utils = @import("utils.zig");
 const context = @import("context.zig");
+const renderer = @import("renderer.zig");
+const sequences = @import("sequences.zig");
+const utils = @import("utils.zig");
 
-var is_resized = std.atomic.Value(bool).init(false);
+const c = @cImport({
+    @cInclude("signal.h");
+});
+
+var isResized = std.atomic.Value(bool).init(false);
 
 fn sigWinchHandler(sig: i32) callconv(.c) void {
     _ = sig;
-    is_resized.store(true, .seq_cst);
+    isResized.store(true, .seq_cst);
 }
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
+    _ = allocator;
 
-    var buf: [1024]u8 = undefined;
-    var stdout = std.fs.File.stdout().writer(&buf);
+    var stdoutBuf: [1024]u8 = undefined;
+    var stdout = std.fs.File.stdout().writer(&stdoutBuf);
     const writer = &stdout.interface;
 
     var act: std.posix.Sigaction = .{
@@ -29,22 +35,25 @@ pub fn main() !void {
     };
     std.posix.sigaction(std.posix.SIG.WINCH, &act, null);
 
-    const size = try utils.getWinSize();
+    var winchOnly = std.posix.sigemptyset();
+    std.posix.sigaddset(&winchOnly, std.posix.SIG.WINCH);
+    std.posix.sigprocmask(std.posix.SIG.BLOCK, &winchOnly, null);
+
+    const waitMask = std.posix.sigemptyset();
 
     var renderContext: context.RenderContext = .{};
 
-    try renderer.render(allocator, &renderContext, size, writer);
-    try writer.flush();
+    try sequences.disableAutoWrap(writer);
 
-    // while (true) {
-    //     // if (is_resized.load(.seq_cst)) {
-    //     //     is_resized.store(false, .seq_cst);
+    var size = try utils.getWinSize();
+    try renderer.render(&renderContext, size, writer);
 
-    //     //     size = try utils.getWinSize();
+    while (true) {
+        _ = c.sigsuspend(@ptrCast(&waitMask));
 
-    //     //     try renderer.render(size, writer);
-    //     // }
-
-    //     std.Thread.sleep(100 * std.time.ns_per_ms);
-    // }
+        if (isResized.swap(false, .seq_cst)) {
+            size = try utils.getWinSize();
+            try renderer.render(&renderContext, size, writer);
+        }
+    }
 }
--- src/renderer.zig
+++ src/renderer.zig
@@ -7,16 +7,25 @@ const RenderContext = contextMod.RenderContext;
 const sequences = @import("sequences.zig");
 const utils = @import("utils.zig");
 
-pub fn render(
-    childAllocator: Allocator,
-    context: *RenderContext,
-    size: utils.WinSize,
-    writer: *Writer,
-) !void {
-    var arena = std.heap.ArenaAllocator.init(childAllocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-    _ = allocator;
+pub fn render(context: *RenderContext, size: utils.WinSize, writer: *Writer) !void {
+    try renderUI(context, size, writer);
+    try writer.flush();
+
+    // i = 0;
+    // while (i < size.col - 2) : (i += 1) {
+    //     std.Thread.sleep(100 * std.time.ns_per_ms);
+    //     try sequences.setCursorPos(context, 1, i + 2, writer);
+    //     try writer.writeByte('=');
+    //     try sequences.setCursorPos(context, 2, 2, writer);
+    // }
+
+    // try writer.writeByte('\n');
+    // try writer.flush();
+}
+
+fn renderUI(context: *RenderContext, size: utils.WinSize, writer: *Writer) !void {
+    try sequences.setCursorPos(context, 1, 1, writer);
+    try sequences.eraseDisplayAfterCursor(writer);
 
     try writer.writeByte('<');
     var i: usize = 0;
@@ -25,16 +34,4 @@ pub fn render(
     }
     try sequences.writeAscii(context, ">\n[ ]", writer);
     try sequences.setCursorCol(2, writer);
-    try writer.flush();
-
-    i = 0;
-    while (i < size.col - 2) : (i += 1) {
-        std.Thread.sleep(100 * std.time.ns_per_ms);
-        try sequences.setCursorPos(context, 1, i + 2, writer);
-        try writer.writeByte('=');
-        try sequences.setCursorPos(context, 2, 2, writer);
-    }
-
-    try writer.writeByte('\n');
-    try writer.flush();
 }
