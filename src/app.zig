21562f8d851e7932e9eaa6dfe2785b012cd4372a
Jackson Otto <jacksonotto120@gmail.com>
2026-04-22 23:20:27 -0700

    test

--- src/app.zig
+++ src/app.zig
@@ -9,27 +9,47 @@ const ui = @import("ui.zig");
 const utils = @import("utils.zig");
 
 pub fn renderUI(terminal: *terminalMod.Terminal) !ui.UIElement {
-    // const widthText = try std.fmt.allocPrint(terminal.allocator, "w: {d}", .{terminal.size.col});
-    // const heightText = try std.fmt.allocPrint(terminal.allocator, "h: {d}", .{terminal.size.row});
+    const allocator = terminal.allocator;
+    const area: u32 = @as(u32, terminal.size.col) * @as(u32, terminal.size.row);
 
-    // var wBox = ui.Text.fromConstText(widthText);
-    // _ = wBox.styles.border(.Rounded).paddingX(1).italic();
+    const widthText = try std.fmt.allocPrint(allocator, "w: {d}", .{terminal.size.col});
+    const heightText = try std.fmt.allocPrint(allocator, "h: {d}", .{terminal.size.row});
+    const areaText = try std.fmt.allocPrint(allocator, "area: {d}", .{area});
 
-    // var hBox = ui.Text.fromConstText(heightText);
-    // _ = hBox.styles.underline().bold().border(.Square);
+    var wBox = ui.Text.fromConstText(widthText);
+    _ = wBox.styles.border(.Rounded).paddingX(1).italic();
 
-    // const layout = try ui.Layout.fromElements(
-    //     terminal.allocator,
-    //     &[_]ui.UIElement{ wBox, hBox },
-    //     if (terminal.size.col % 2 == 0) .Vertical else .Horizontal,
-    // );
+    var hBox = ui.Text.fromConstText(heightText);
+    _ = hBox.styles.border(.Square).paddingX(1).bold();
 
-    // return layout;
+    var aBox = ui.Text.fromConstText(areaText);
+    _ = aBox.styles.border(.Rounded).paddingX(1).underline();
 
-    _ = terminal;
-    var box = ui.Text.fromConstText("");
-    _ = box.styles.border(.Rounded);
-    return box;
+    const topRow = try ui.Layout.fromElements(
+        allocator,
+        &[_]ui.UIElement{ wBox, hBox, aBox },
+        .Horizontal,
+    );
+
+    var block = ui.Text.fromConstText("line one\nline two is longer\nthird");
+    _ = block.styles.border(.Square).padding(1).bold();
+
+    const plain = ui.Text.fromConstText("plain text, no styles");
+
+    var styledLine = ui.Text.fromConstText("bold+italic+underline");
+    _ = styledLine.styles.bold().italic().underline();
+
+    const bottomRow = try ui.Layout.fromElements(
+        allocator,
+        &[_]ui.UIElement{ plain, styledLine },
+        if (terminal.size.col % 2 == 0) .Vertical else .Horizontal,
+    );
+
+    return try ui.Layout.fromElements(
+        allocator,
+        &[_]ui.UIElement{ topRow, block, bottomRow },
+        .Vertical,
+    );
 }
 
 pub const mockConfig: config.Config = .{
